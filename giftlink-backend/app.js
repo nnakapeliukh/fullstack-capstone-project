1ff4f4314ca2cd298f49f7e1d5a513b76e97fd64
Nazar <nnakapeliukh@gmail.com>
2024-04-14 21:33:03 -0500

    Adds search routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,8 +7,6 @@ const pinoLogger = require("./logger");
 const connectToDatabase = require("./models/db");
 const { loadData } = require("./util/import-mongo/index");
 
-const giftroutes = require("./routes/giftRoutes");
-
 const app = express();
 app.use("*", cors());
 const port = 3060;
@@ -24,10 +22,10 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftroutes = require("./routes/giftRoutes");
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require("pino-http");
 const logger = require("./logger");
@@ -38,7 +36,7 @@ app.use(pinoHttp({ logger }));
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use("/api/gifts", giftroutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
