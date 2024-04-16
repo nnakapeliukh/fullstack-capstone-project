9e7d6c2c5b6b9d3c8bc9a2dee4088b92d37b849e
Nazar <nnakapeliukh@gmail.com>
2024-04-16 06:31:46 -0500

    Updates frontend

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,4 +1,6 @@
 const connectToDatabase = require("../models/db");
+const express = require("express");
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
@@ -31,7 +33,7 @@ router.get('/:id', async (req, res) => {
 
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
         const gift = await collection.findOne({ id: id });
-        
+
         if (!gift) {
             return res.status(404).send('Gift not found');
         }
--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">GiftLink</Link>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    {/* Task 1: Add links to Home and Gifts below*/}
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        GiftLink
+      </Link>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          {/* Task 1: Add links to Home and Gifts below*/}
+          <li className="nav-item">
+            <Link className="nav-link" to="/home.html">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/app">
+              Gifts
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
